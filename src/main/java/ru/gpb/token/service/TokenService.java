1da521f76d50f020a64648eb5de7724c4401214c
Alexey Sen <alexey.sen@gmail.com>
2023-08-06 13:51:31 +0300

    Уточнены форматы запроса и возврата токена (расширение состава данных).

--- src/main/java/ru/gpb/token/service/TokenService.java
+++ src/main/java/ru/gpb/token/service/TokenService.java
@@ -2,7 +2,6 @@ package ru.gpb.token.service;
 
 import ru.gpb.token.entity.Dto.TokenRequest;
 import ru.gpb.token.entity.Dto.TokenResponse;
-import ru.gpb.token.entity.TokenType;
 
 public interface TokenService {
     String secret2Token(TokenRequest request);
